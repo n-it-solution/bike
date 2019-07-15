fd18577563b96f2d010d242d9b338be8756b6c77
Abdul Manan <39721062+mirza339@users.noreply.github.com>
2019-07-16 00:39:50 +0500

    bike

--- src/Controller/Admin/bikeController.php
+++ src/Controller/Admin/bikeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Bike;
+use App\Entity\Company;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class bikeController extends AbstractController
@@ -10,10 +13,39 @@ class bikeController extends AbstractController
     /**
      * @Route("/admin/bike", name="admin_bike")
      */
-    public function index()
+    public function index(Request $request)
     {
+        if($request->getMethod() == 'POST'){
+            $bike = New Bike();
+            $bike->setName($request->get('name'));
+            $bike->setPrice($request->get('price'));
+            if($_FILES['doc']['name'] != ""){
+                $info = pathinfo($_FILES['doc']['name']);
+                $ext = $info['extension'];
+                $date = date('mdYhisms', time());
+                $newname = $date . '.' . $ext;
+                $target = 'upload/'.$newname;
+                move_uploaded_file( $_FILES['doc']['tmp_name'], "./".$target);
+                $bike->setPic($target);
+            }
+            $bike->setCompany($this->getDoctrine()->getRepository(Company::class)->find($request->get('company')));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($bike);
+            $em->flush();
+            return $this->redirectToRoute('admin_bike');
+        }
+        $bikes = $this->getDoctrine()->getRepository(Bike::class)->findAll();
+        $company = $this->getDoctrine()->getRepository(Company::class)->findAll();
         return $this->render('admin/bike/index.html.twig', [
-            'controller_name' => 'bikeController',
+            'bikes' => $bikes,
+            'company' => $company
         ]);
     }
+
+//    /**
+//     * @Route("/admin/bike/action/{action}", name="admin_bike_action")
+//     */
+//    public function bike(){
+//
+//    }
 }
---
+++ src/Entity/Bike.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BikeRepository")
+ */
+class Bike
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Pic;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Price;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Company")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $Company;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getPic(): ?string
+    {
+        return $this->Pic;
+    }
+
+    public function setPic(string $Pic): self
+    {
+        $this->Pic = $Pic;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->Price;
+    }
+
+    public function setPrice(int $Price): self
+    {
+        $this->Price = $Price;
+
+        return $this;
+    }
+
+    public function getCompany(): ?Company
+    {
+        return $this->Company;
+    }
+
+    public function setCompany(?Company $Company): self
+    {
+        $this->Company = $Company;
+
+        return $this;
+    }
+}
